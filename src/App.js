128d87fa957579d068187f1a6af8e0a57b808889
Mayank Kumar <Mayankkumaryan1@gmail.com>
2025-09-23 23:00:17 +0530

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import { useState } from "react"
 import CounterText from "./CounterText"
-
+import One from "./components/One";
+import Two from "./components/Two";
+import Three from "./components/Three";
 const App = () => {
-  const [counter, setCounter] = useState(0);
 
-  const increment = () => {
-    setCounter((prevCounter) => prevCounter + 1);
-    setCounter((prevCounter) => prevCounter + 1);
+  //const [counter, setCounter] = useState(0);
+  const [currentShow, setCurrentShow] = useState(0);
+
+  const showOne = () => {
+    setCurrentShow(1);
   }
 
-  const decrement = () => {
-    setCounter((prevCounter) => prevCounter - 1);
+  const showTwo = () => {
+    setCurrentShow(2);
+  }
+
+  const showThree = () => {
+    setCurrentShow(3);
   }
   return (
     <>
+      {/*
       <CounterText counter={counter} />
       <CounterText counter={counter} />
       <CounterText counter={counter} />
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      */}
+      
+      <button onClick={showOne}>Show One</button>
+      <button onClick={showTwo}>Show Two</button>
+      <button onClick={showThree}>Show Three</button>
+      {/*
+      {currentShow === 1 && <One />}
+      {currentShow === 2 && <Two />}
+      {currentShow === 3 && <Three />}
+      */}
+      {currentShow === 1 ? <One /> : <Two />}
+      {currentShow === 2 ? <Two /> : null}
+      {currentShow === 3 ? <Three /> : null}
+      <One />
+      <Two />
+      <Three />
     </>
   )
 }
