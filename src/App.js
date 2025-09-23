ee71be9fb79037baab1525d46e25301035d366ab
Mayank Kumar <Mayankkumaryan1@gmail.com>
2025-09-23 10:31:48 +0530

    Add Counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
-function App() {
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+  }, []);
+
+  const increment = () => {
+    //setCounter(counter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>This is Counter Number: {counter}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+    </>
+  )
 }
 
-export default App;
+export default App
